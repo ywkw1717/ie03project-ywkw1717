a85124a33eafcb2f449ac4c8e3c33f1d8546a894
yyy <y.w.k.w.1717@gmail.com>
2018-04-25 03:05:49 +0900

    finish assignment 1

--- detect_similar_users.go
+++ detect_similar_users.go
@@ -6,26 +6,31 @@ import (
   "bufio"
   "strings"
   "strconv"
+  "math"
+  "sort"
 )
+
+var s = bufio.NewScanner(os.Stdin)
 
 type UserBookPair [2]int
 
-func (ubp UserBookPair) UserNum() int {
+func (ubp UserBookPair) userNum() int {
   return ubp[0]
 }
 
-func (ubp UserBookPair) BookNum() int {
+func (ubp UserBookPair) bookNum() int {
   return ubp[1]
 }
 
-func main() {
-  s := bufio.NewScanner(os.Stdin)
-
+func nextLine() string {
   s.Scan()
+  return s.Text()
+}
 
-  inputValue := strings.Split(s.Text(), " ")
+func main() { inputValue := strings.Split(nextLine(), " ")
   ubp := UserBookPair{}
 
+  // get the number of user and book
   for i, v := range inputValue {
     if num, err := strconv.Atoi(v); err != nil {
       fmt.Println(err)
@@ -35,6 +40,47 @@ func main() {
     }
   }
 
-  fmt.Println(ubp.UserNum())
-  fmt.Println(ubp.BookNum())
+  data   := make([][]float64, ubp.userNum())
+  result := make([][]float64, ubp.userNum() - 1)
+
+  // get the all evaluation
+  for i := 0; i < ubp.userNum(); i += 1 {
+    inputValue := strings.Split(nextLine(), " ")
+    for _, v := range inputValue {
+      if num, err := strconv.ParseFloat(v, 64); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+      } else {
+        data[i] = append(data[i], num)
+      }
+    }
+  }
+
+  // calculate degree of similarity
+  LOOP:
+  for i, v := range data {
+    var sum float64
+    for j, w := range v {
+      if i == 0 {
+        continue LOOP
+      }
+
+      if data[0][j] == -1 {
+        continue
+      } else if w == -1 {
+        continue
+      } else {
+        sum += math.Pow(math.Abs(data[0][j] - w), 2)
+      }
+    }
+    result[i - 1] = []float64{float64(i + 1), 1/(math.Sqrt(sum) + 1)}
+  }
+
+  // sort
+  sort.Slice(result, func(i, j int) bool { return result[i][1] > result[j][1] })
+
+  // output results
+  for _, v := range result {
+    fmt.Println(v[0], v[1])
+  }
 }
